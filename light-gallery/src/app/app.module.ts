daa563d6bf90e1e627fece1b5a47eabf47277ea0
Ben <danquahwhite@gmail.com>
2022-04-30 12:57:01 +0000

    init

---
+++ light-gallery/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Box1Component } from './box1/box1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Box1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
